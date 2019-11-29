d4f0e632774e3477a0a8b61289b333b809761780
ryan susman <ryansusman@gmail.com>
2019-11-29 14:49:36 -0600

    Clean up

--- EnemyDetector/Scripts/Run.py
+++ EnemyDetector/Scripts/Run.py
@@ -1,20 +1,29 @@
 import pyautogui
 import numpy as np
 import tensorflow.keras as keras
+import ctypes
 
 # Load Model
 model = keras.models.load_model('..\\Models\\CODV4.h5')
 
+# Find Center Of Screen
+user32 = ctypes.windll.user32
+screenSize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+centerPoint = tuple(i/2 for i in screenSize)
+print('Screen Size X:%d y:%d' % screenSize)
+print('Targeting Center X:%d y:%d' % centerPoint)
+
+# Starting Main Loop
 print('Started')
 while 1 == 1:
     # Grab Screen
-    im = pyautogui.screenshot(region=(860, 440, 200, 200))
-
+    image = pyautogui.screenshot(region=(centerPoint[0] - 200, centerPoint[1] - 200, 200, 200))
+    image.show()
     # Format and Normalize Data
-    ReducedGrayScaleNPNeutralImages = np.asarray([np.asarray(im)]) / 255
+    normalizedImage = np.asarray([np.asarray(image)]) / 255
 
     # Predict
-    prediction = model.predict(ReducedGrayScaleNPNeutralImages)
+    prediction = model.predict(normalizedImage)
 
     if prediction[0][0] > 0.99:
         print('Enemy!')
