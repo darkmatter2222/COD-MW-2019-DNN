968bec0e2093f0f652dbb5f8f0cb1146bb484c2d
ryan susman <ryansusman@gmail.com>
2019-11-30 16:09:52 -0600

    overlay

--- EnemyDetector/Scripts/Overlay.py
+++ EnemyDetector/Scripts/Overlay.py
@@ -3,34 +3,50 @@ import ctypes
 import pyautogui
 import numpy as np
 import tensorflow.keras as keras
+from datetime import datetime
+from datetime import timedelta
 
 # Load Model
-model = keras.models.load_model('..\\Models\\CODV4.h5')
+model = keras.models.load_model('..\\Models\\CODV5.h5')
 
 # Find Center Of Screen
 user32 = ctypes.windll.user32
 screenSize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 centerPoint = tuple(i/2 for i in screenSize)
 
+
+
 root = Tk()
 v = StringVar()
-flashLabel = Label(root, textvariable=v, background='#ffffff', foreground='red', font=("Helvetica bold", 23), width=400)
+flashLabel = Label(root, textvariable=v, background='black', foreground='red', font=("Helvetica bold", 50), width=1000)
 flashLabel.pack()
 
 v.set("Woot")
 
 def task():
+    lastSecondTimestamp = datetime.utcnow() + timedelta(seconds=1)
+    tickCounter = 0
     while 1 == 1:
+        tickCounter += 1
+
+        if datetime.utcnow() > lastSecondTimestamp:
+            lastSecondTimestamp = datetime.utcnow() + timedelta(seconds=1)
+            print('Ticks Per Second %d' % tickCounter)
+            tickCounter = 0
+
+
         # Grab Screen
         image = pyautogui.screenshot(region=(centerPoint[0] - 100, centerPoint[1] - 100, 200, 200))
         # Predict
         prediction = model.predict(np.asarray([np.asarray(image)]) / 255)
         # Print Result
         if prediction[0][0] > 0.95:
-            v.set("Enemy %d" % round(prediction[0][0] * 100, 5))
+            v.set("Enemy " + str(round(prediction[0][0] * 100, 2)).zfill(6) + '% Confident')
+            flashLabel.configure(foreground="red")
             flashLabel.update()
         else:
-            v.set("")
+            v.set("Enemy " + str(round(prediction[0][0] * 100, 2)).zfill(6) + '% Confident')
+            flashLabel.configure(foreground="green")
             flashLabel.update()
 
 
@@ -38,7 +54,7 @@ root.overrideredirect(True)
 root.attributes('-topmost', True)
 root.wm_attributes("-transparentcolor", "#ffffff")
 root.configure(background='#ffffff')
-root.geometry('%dx%d+%d+%d' % (400, 400, centerPoint[0] - (100 * 1), centerPoint[1] - (300 * 1)))
+root.geometry('%dx%d+%d+%d' % (1000, 1000, centerPoint[0] - (500 * 1), centerPoint[1] - (500 * 1)))
 root.title("Welcome to LikeGeeks app")
 
 root.after(2000, task)
